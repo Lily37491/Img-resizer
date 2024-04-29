c38d3d9f11e1439ff06aa0acddc9368ffc015102
Payal Kumavat <payalkumavat02@gmail.com>
2024-04-29 16:58:47 +0530

    Add image resizing functionality with Pillow

--- api/app.py
+++ api/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from PIL import Image
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hi There!!!"
+    with Image.open(r"C:\Users\payal\Downloads\Cheesecake.jpg") as img:
+        resize = img.resize((1080,1080), Image.LANCZOS)
+    return resize.show()
 
 if __name__== "__main__":
     app.run(debug=True)
